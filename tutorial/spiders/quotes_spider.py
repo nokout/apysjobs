c1b7f1ae0b62359499a89938185f8a8431c57821
nokout <nigel.okeefe@gmail.com>
2017-01-03 11:43:05 +1100

    clean up comments and redundant code

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -15,30 +15,14 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
 
-        request1 = scrapy.Request(
+        # This initial request basically just sets up the session
+        #   and page size before we do the search.
+        yield scrapy.Request(
             url="""https://www.apsjobs.gov.au/quickSearch.aspx?mn=JobSearch&ifm=true""",
             callback=self.setup_parse,
-            cookies= {'UserPreferencesPageSize': "PageSize=1000"})
-
-        return [request1]
+            cookies={'UserPreferencesPageSize': "PageSize=1000"})
 
     def setup_parse(self, response):
-        data = {
-            '__EVENTARGUMENT': '',
-            '__LASTFOCUS': '',
-            '__VIEWSTATE': '',
-            '__SCROLLPOSITIONX': '271',
-            '__SCROLLPOSITIONY': '403',
-            '__VIEWSTATEENCRYPTED': '',
-            'ctl00$ContentPlaceHolderSite$btnSearch': 'Search'
-        }
-
-        # url="""https://www.apsjobs.gov.au/quickSearch.aspx?mn=JobSearch&ifm=true""",
-        #                                        cookies={'UserPreferencesCookieCheck': 'APSjobs',
-        #                                                 'UserPreferencesPageSize': '1000',
-        #                                                 'APSjobs_Session': 'vulbbuk55xezodowvzbb01ce'},
-        with open('pre_search_result.html', 'wb') as f:
-            f.write(response.body)
         headers = {
             'Host': 'www.apsjobs.gov.au',
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
@@ -49,39 +33,38 @@ class QuotesSpider(scrapy.Spider):
             'Upgrade-Insecure-Requests': '1',
             'Pragma': 'no-cache',
             'Cache-Control': 'no-cache',
-            }
-
-        data = {
-          '__EVENTTARGET': '',
-          '__EVENTARGUMENT': '',
-          '__VIEWSTATE': '',
-          '__SCROLLPOSITIONX': '400',
-          '__SCROLLPOSITIONY': '119',
-          '__VIEWSTATEENCRYPTED': '',
-          'ctl00$ContentPlaceHolderSite$ucAgency': '',
-          'ctl00$ContentPlaceHolderSite$ucJobCategory': '',
-          'ctl00$ContentPlaceHolderSite$ucClassification': '',
-          'ctl00$ContentPlaceHolderSite$ucState': '',
-          'ctl00$ContentPlaceHolderSite$txtKeywords': '',
-          'ctl00$ContentPlaceHolderSite$txtMinSalary': '',
-          'ctl00$ContentPlaceHolderSite$txtMaxSalary': '',
-          'ctl00$ContentPlaceHolderSite$btnSearch': 'Search',
-          'ctl00$ContentPlaceHolderSite$hMessage': ''
         }
 
-        # requests.post('https://www.apsjobs.gov.au/quickSearch.aspx?mn=JobSearch&ifm=true', headers=headers, cookies=cookies, data=data)
+        data = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': '',
+            '__SCROLLPOSITIONX': '400',
+            '__SCROLLPOSITIONY': '119',
+            '__VIEWSTATEENCRYPTED': '',
+            'ctl00$ContentPlaceHolderSite$ucAgency': '',
+            'ctl00$ContentPlaceHolderSite$ucJobCategory': '',
+            'ctl00$ContentPlaceHolderSite$ucClassification': '',
+            'ctl00$ContentPlaceHolderSite$ucState': '',
+            'ctl00$ContentPlaceHolderSite$txtKeywords': '',
+            'ctl00$ContentPlaceHolderSite$txtMinSalary': '',
+            'ctl00$ContentPlaceHolderSite$txtMaxSalary': '',
+            'ctl00$ContentPlaceHolderSite$btnSearch': 'Search',
+            'ctl00$ContentPlaceHolderSite$hMessage': ''
+        }
 
-        main_request =  scrapy.http.FormRequest.from_response(response,
-                                                    formname="aspnetForm",
-                                                    formdata=data,
-                                                    headers=headers,
-                                                    # url='''https://www.apsjobs.gov.au/quickSearch.aspx?mn=JobSearch&ifm=true''',
-                                                    clickdata={'name': 'ctl00$ContentPlaceHolderSite$btnSearch'},
-                                                    callback=self.get_search_results)
+        #TODO Put cookies in construction and change to a yield instead of return
+        main_request = scrapy.http.FormRequest.from_response(response,
+            formname="aspnetForm",
+            formdata=data,
+            headers=headers,
+            # url='''https://www.apsjobs.gov.au/quickSearch.aspx?mn=JobSearch&ifm=true''',
+            clickdata={
+             'name': 'ctl00$ContentPlaceHolderSite$btnSearch'},
+            callback=self.get_search_results)
+
         main_request.cookies['UserPreferencesPageSize'] = "PageSize=1000"
 
-        print("Request Body... " + str(main_request.body))
-        print("Request Body... " + str(main_request.headers))
         return [main_request]
 
     def get_search_results(self, response):
@@ -106,10 +89,10 @@ class QuotesSpider(scrapy.Spider):
             '#ctl00_ContentPlaceHolderSite_lvSearchResults_gvListView').css('.item, .altItem').extract()
         self.log('Notices Found: {}'.format(len(rows)))
         for i, row in enumerate(rows):
+            url = Selector(text=row).css(
+                'td:nth-child(2)').css('a::attr(href)').extract()[-1]
             id = re.search('Notices=([0-9]*)', Selector(text=row).css(
                 'td:nth-child(2)').css('a::attr(href)').extract_first()).group(1)
-            # url = Selector(text=row).css(
-            #     'td:nth-child(2)').css('a::attr(href)').extract()[-1]
             # date = Selector(text=row).css('td:nth-child(3)::text').extract_first().split(':')[-1].strip().css(
             # '#ctl00_c_ucNoticeDetails_ucNoticeView_tbrSalary').css('td:nth-child(2)::text').extract_first()
             # self.notices['id'] = {'id': id, 'url': url, 'data,
@@ -123,39 +106,3 @@ class QuotesSpider(scrapy.Spider):
 
         with open('''./json/{}.json'''.format(filename), 'wb') as f:
             f.write(json.dumps(build_object(body), indent=4))
-
-# notice_type_text = Selector(response=response).css('.noticeType::text').extract_first().strip()
-# portfolio = Selector(response=response).css('#ctl00_c_ucNoticeDetails_tdPortfolio::text').extract_first().strip()
-# agency = Selector(response=response).css('.agency::text').extract_first().strip()
-# closing_date = Selector(response=response).css('.closingDate::text').extract_first().split(':')[-1].strip()
-# job_title = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrPositionTitle').css('td:nth-child(2)::text').extract_first().strip()
-# branch = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrBranch').css('td:nth-child(2)::text').extract_first().strip()
-# section = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrSection').css('td:nth-child(2)::text').extract_first().strip()
-# job_type = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrPositionType').css('td:nth-child(2)::text').extract_first().split(',')
-# salary_text = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrSalary').css('td:nth-child(2)::text').extract_first()
-# salary_min = salary_text.split('-')[0].replace('$','').replace(',','').strip()
-# salary_max = salary_text.split('-')[-1].replace('$','').replace(',','').strip()
-# locations_text = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_ucToClassificationsLabel_tbrLocation').css('td:nth-child(2)::text').extract_first()
-# ##TODO: Split locations
-# classification = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_ucToClassificationsLabel_tbrClassification').css('td:nth-child(2)::text').extract_first().strip()
-# ##TODO: Split classifications
-# employment_act = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_ucToClassificationsLabel_tbrAgencyAct').css('td'#ctl00_c_ucNoticeDetails_ucNoticeView_lnkAgencyRecruitment::attr(href)':nth-child(2)::text').extract_first().strip()
-# position_id = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrPositionNo').css('td:nth-child(2)::text').extract_first().strip()
-# agency_website = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrAgencyUrlTop').css('td:nth-child(2) a::attr(href)').extract_first().strip()
-#
-# ##TODO Extract Position Description
-# ##     Not all contained in a div, is in same container as innertables
-#
-# selection_documentation = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrSelectionDocumentation').css('td:nth-child(2)::text').extract_first().strip()
-# position_contact = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrContactDetails').css('td:nth-child(2)::text').extract_first().strip()
-# agency_website_recruitment = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_tbrAgencyRecruitmentUrl').css('td:nth-child(2)::text').extract_first().strip()
-# agency_website_apply_function_text = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_lnkApply::attr(onclick)').extract_first().strip()
-# agency_website_information = Selector(response=response).css('#ctl00_c_ucNoticeDetails_ucNoticeView_lnkAgencyRecruitment::attr(href)').extract_first().strip()
-# notes_html = Selector(response=response).css('#ctl00_c_ucNoticeDetails_pnlNotes > div.notes').extract()
-#
-#
-#
-
-# self.log('x.Sastrip() for x in ved file
-# {}'.format(filename).css('#ctl00_c_ucNoticeDetails_pnlNotes
-# div::text').extract()for.css('#ctl00_c_ucNoticeDetails_divStaticEmploymentOpportunityText::text').extract_first().strip()]
